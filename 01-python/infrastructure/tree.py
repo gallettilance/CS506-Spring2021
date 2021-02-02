c639ece7a1ca977e76450da04b49e3625f2bff3e
Nicholas <njmosca@bu.edu>
2021-02-02 17:21:26 -0500

    Built a tree

--- 01-python/infrastructure/tree.py
+++ 01-python/infrastructure/tree.py
@@ -1,3 +1,14 @@
 def draw_tree():
-    print("tree not found")
-    return
+    ''' Making a simple Tree''' 
+
+    for i in range(5):
+        for s in range(-6, -i):
+            print(" ", end="")
+        for j in range(i+1):
+            print("* ", end="")
+        print()
+    print(' ',' ',' | | ',' ',' ')
+    print(' ',' ',' | | ',' ',' ')
+   
+
+    
