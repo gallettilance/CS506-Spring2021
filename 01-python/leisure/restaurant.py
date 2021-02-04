99c92c5edf82ffa96ce3501ac2845942e0f71048
Mingcheng Xu <mcx@DESKTOP-6A7Q3O1.localdomain>
2021-02-04 16:28:09 +0800

    add randomness

--- 01-python/leisure/restaurant.py
+++ 01-python/leisure/restaurant.py
@@ -1,3 +1,13 @@
+import random
+
+def randomSpecial():
+    special = ["Lobster Spaghetti", "Beef Wellington", "Peking Duck", "Sukiyaki", "Sichuan Hotpot", "Big Mac"]
+    return random.choice(special)
+
 def draw_restaurant():
-    print("restaurant not found")
+    print("This is an All-you-can-eat restaurant providing all kinds of tasty food. Free for BU members!")
+    print("daily menu: Error: the file is too big, DATA OVERFLOW!")
+    print("Though the menu explodes due to technique problems out of kitchen, our Chief still remembers to offer today's special.")
+    ts = randomSpecial()
+    print("Today's special is: "+ ts + ". CHEERS!")
     return
