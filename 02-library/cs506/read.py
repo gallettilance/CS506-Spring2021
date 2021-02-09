4ec12c753457b7a3dca9296a35b25ff4003756fa
Audrey McMillion <amcmi@bu.edu>
2021-02-09 16:44:50 -0500

    adding lab2 files read.py and sim.py

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,19 @@
+def convert_type(x):
+    types = [int, float]
+    for t in types:
+        try:
+            ret = t(x)
+            return ret
+        except ValueError:
+            pass
+    return str(x).strip('\"')
+
 def read_csv(csv_file_path):
-    """
-        Given a path to a csv file, return a matrix (list of lists)
-        in row major.
-    """
-    raise NotImplementedError()
+    matrix = []
+    with open(csv_file_path, "r") as f:
+        for l in f.readlines():
+            line = []
+            for m in l.strip('\n').split(','):
+                line.append(convert_type(m))
+            matrix.append(line)
+    return matrix
