6325511af7921b750157491f41742939b29a12d9
YichenMu <ycmu@bu.edu>
2021-02-10 21:07:50 +0800

    Modified read.py

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,28 @@
+def convert_type(item):
+    types=[int,float]
+    for type in types:
+        try:
+            return type(item)
+        except ValueError:
+            return item.strip('\"')
+
+    
+
+
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    result=[]
+    with open (csv_file_path, 'r') as file:
+        
+        lines = file.readlines()
+        for line in lines:
+            dataset = line.rstrip().split(sep=',')
+            row=[]
+            for data in dataset:
+                row.append(convert_type(data))
+            result.append(row)
+        return result 
+        
