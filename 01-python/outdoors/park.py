fd49678898ba95460b02871bf3dd7904d2e1eb35
YichenMu <77951154+YichenMu@users.noreply.github.com>
2021-02-13 10:56:27 +0800

    I have added a few more choices to parkchoices.
    Hello professor! I am Yichen Mu. I have added a few more choices to parkchoices. Since I have '&' in my branch name, I couldn't get access to the 'tree&park' branch using terminal. So I modified the file using Pycharm and made sure it can run successfully, and then I modified this file on the Github editor directly. Please review and check. Thank you in advance!

--- 01-python/outdoors/park.py
+++ 01-python/outdoors/park.py
@@ -1,5 +1,5 @@
 import random
 def draw_park():
-    parkchoices=['theme','central','industrial']
-    print("This is a(n) "+random.choice(parkchoices)+' park! Enjoy your time!')
+    parkchoices=['theme','central','industrial','neighbourhood park','Sequoia National Park','Yellowstone National Park','Great Smoky Mountains National Park','Rocky Mountain National Park']
+    print("This is a(n) "+random.choice(parkchoices)+"! Enjoy your time!")
     return
