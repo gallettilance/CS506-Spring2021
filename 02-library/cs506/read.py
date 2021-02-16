78ba076c0bb1f92431b798a5f71abaa216aec1d7
CZXgodlike <997232782@qq.com>
2021-02-16 18:32:46 -0500

    implemented read.py and sim.py

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,24 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    X = []
+    try:
+        f = open(csv_file_path, 'r')
+
+    except OSError as err:
+        print(err)
+        return
+
+    with f:
+        for row in f:
+            X.append(row.split(','))
+
+    return X
+
+
+if __name__ == '__main__':
+    f = open("../tests/test_files/dataset_2.csv", 'r')
+    for row in f:
+        #print(row.split(','))
+        print(row.split(',')[0])
+        #print(type(row.split(',')[1]))
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,16 +1,34 @@
 def euclidean_dist(x, y):
     res = 0
     for i in range(len(x)):
-        res += (x[i] - y[i])**2
-    return res**(1/2)
+        res += (x[i] - y[i]) ** 2
+    return res ** (1 / 2)
+
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    res = 0
+    for i in range(len(x)):
+        res += (x[i] - y[i])
+    return res
+    # raise NotImplementedError()
+
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    intersection = len(list(set(x).intersection(set(y))))
+    union = (len(x) + len(y)) - intersection
+    return 1 - float(intersection) / union
+
+
+# raise NotImplementedError()
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    dot = sum(a * b for a, b in zip(x, y))
+    norm_x = sum(a * a for a in x) ** 0.5
+    norm_y = sum(b * b for b in y) ** 0.5
+    return dot / (norm_x * norm_y)
 
-# Feel free to add more
+
+def jaccard_sim(x, y):
+    intersection = len(list(set(x).intersection(set(y))))
+    union = (len(x) + len(y)) - intersection
+    return float(intersection) / union
