e0c34c86dd13f6f23ff2566f54f5ad08048e284f
Waner Zhou <zwe@bu.edu>
2021-02-19 22:49:02 -0500

    half-way on the 02-library workshop

--- 02-library/cs506/kmeans.py
+++ 02-library/cs506/kmeans.py
@@ -11,7 +11,17 @@ def point_avg(points):
     
     Returns a new point which is the center of all the points.
     """
-    raise NotImplementedError()
+    dimensino_point = len(points[0])
+    for pt in points:
+        if len(pt) != dimensino_point:
+            raise ValueError("points are not in the same dimension")
+    res = ()
+    for j in range(dimensino_point):
+        x = [p[j] for p in points]
+        res.append(sum(x)/dimensino_point)
+    return res
+
+        
 
 
 def update_centers(dataset, assignments):
@@ -21,7 +31,14 @@ def update_centers(dataset, assignments):
     Compute the center for each of the assigned groups.
     Return `k` centers in a list
     """
-    raise NotImplementedError()
+    res = []
+    for l in assignments:
+        curlist = [elem for elem in l]
+        cur_points = [dataset[i] for i in curlist]
+        res.append(point_avg(cur_points))
+
+    return res
+
 
 def assign_points(data_points, centers):
     """
@@ -43,20 +60,29 @@ def distance(a, b):
     """
     Returns the Euclidean distance between a and b
     """
-    raise NotImplementedError()
+    res = 0
+    for i in range(len(a)):
+        res += (a[i] - b[i])**2
+    return res**(1/2)
 
 def distance_squared(a, b):
-    raise NotImplementedError()
+    res= distance(a,b)
+    return res**2
 
 def generate_k(dataset, k):
     """
     Given `data_set`, which is an array of arrays,
     return a random set of k points from the data_set
     """
-    raise NotImplementedError()
+    return random.sample(dataset, k)
 
 def cost_function(clustering):
-    raise NotImplementedError()
+    res = 0
+    for cluster in clustering:
+        cur_center = point_avg(cluster)
+        for i in cluster:
+            res+=distance(cur_center, i)
+    return res
 
 
 def generate_k_pp(dataset, k):
@@ -66,7 +92,25 @@ def generate_k_pp(dataset, k):
     where points are picked with a probability proportional
     to their distance as per kmeans pp
     """
-    raise NotImplementedError()
+    selected_centroid = []
+    # first centroid is randomly choosen
+    first_select = random.choice(dataset)
+    selected_centroid.append(first_select)
+    # check the distance and gradually pick the rest
+    while len(selected_centroid)<k:
+        distancelist = []
+        for point in dataset:
+            curdist = []
+            for eachselected in selected_centroid:
+                curdist.append(distance(eachselected,point))
+            curmin = min(curdist)
+            curmin /= curmin.sum()
+            distancelist.append((curmin,point))
+        curselected = random.choice(dataset, p = curmin / np.sum(curmin))
+        selected_centroid.append(curselected)
+
+    return selected_centroid
+
 
 
 def _do_lloyds_algo(dataset, k_points):
@@ -86,7 +130,7 @@ def k_means(dataset, k):
     if k not in range(1, len(dataset)+1):
         raise ValueError("lengths must be in [1, len(dataset)]")
     
-    k_points = generate_k(dataset, k)
+    k_points = generate_k(dataset, k) 
     return _do_lloyds_algo(dataset, k_points)
 
 
