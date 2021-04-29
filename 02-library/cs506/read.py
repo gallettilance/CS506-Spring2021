67f3d3989d31683e28d6e082314ef92759739582
Khaled Sulayman <khaled@thesulaymans.com>
2021-04-28 23:34:15 -0400

    implement read_csv

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,5 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    with open(csv_file_path, 'r') as f:
+        return [line.split(',') for line in f]
